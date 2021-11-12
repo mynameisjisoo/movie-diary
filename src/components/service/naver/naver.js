4ce50acc3c692c1adf189fcd4821f89d9e788664
jisoo <devjisoolee@gmail.com>
2021-11-12 23:36:02 +0900

    set up proxy for cors

---
+++ src/components/service/naver/naver.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+class Naver {
+  constructor(id, secret) {
+    this.id = id;
+    this.secret = secret;
+    this.naver = axios.create({
+      headers: {
+        'X-Naver-Client-Id': this.id,
+        'X-Naver-Client-Secret': this.secret
+      }
+    });
+  }
+
+  async search(query) {
+    this.naver
+      .get('v1/search/movie.json', {
+        params: {
+          display: '20',
+          query: query
+        }
+      })
+      .then(response => {
+        console.log(response);
+        return response.data.items;
+      });
+  }
+}
+export default Naver;
